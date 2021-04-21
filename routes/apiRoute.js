b673d54b28bc9415bf53161009511099c2139d40
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-04-21 14:52:33 +0930

    rewrote api calls as not working on heroku

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,29 +1,24 @@
-const { Workout } = require('../models/workout.js');
-const mongojs = require("mongojs");
 const router = require("express").Router();
+const mongojs = require("mongojs");
+const { Workout } = require('../models/workout.js');
 
 // get info for the workouts page
 router.get("/api/workouts", (req, res) => {
-    Workout.find({}).then(dbWorkout => {
-        const result = dbWorkout.map(workout => {
-            const workoutEl = workout.toObject();
-            workoutEl.totalDuration = workout.exercises.reduce((totalDuration, exercise) => totalDuration + exercise.duration, 0);
-
-            return workout;
+    Workout.find({})
+        .then((dbWorkout) => {
+            res.status(200).json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
         });
-
-        res.status(200).json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    })
 });
+
 
 
 // get info for the range page
 router.get("/api/workouts/range", ({}, res) => {
-    Workout.find({
-            day: { $gte: new Date(new Date().getTime() - (7 * 24 * 60 * 60 * 1000)) }
-        })
+    Workout.find({})
+    limit(10)
         .then((dbWorkout) => {
             res.status(200).json(dbWorkout);
         }).catch(err => {
@@ -32,28 +27,22 @@ router.get("/api/workouts/range", ({}, res) => {
 });
 
 // post submits new completed workouts
-router.post("/api/workouts", (req, res) => {
-    const workout = req.body;
-
-    Workout.create(workout)
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
         .then((result) => {
             res.status(200).json(result);
-        }).catch(err => {
+        }).catch((err) => {
             res.status(400).json(err);
         });
 });
 
 // put to update workouts and update the exercsise body
-router.put("/api/workouts/:id", (req, res) => {
-    const exercise = req.body;
-    const workoutId = req.params.id;
-
-    Workout.findById(workoutId)
-        .then((workout) => {
-            workout.exercises.push(exercise);
-            workout.save().then(res => {
-                res.json(res);
-            });
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+            params.id, { $push: { exercises: body } }, { new: true, }
+        )
+        .then((dbWorkout) => {
+            res.status(200).json(dbWorkout);
         })
         .catch((err) => {
             res.status(400).json(err);
